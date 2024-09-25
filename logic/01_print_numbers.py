6f8330009d8fa0d65ef945d3839df89220b61b46
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-09-25 16:37:55 -0500

    delete

--- logic/01_print_numbers.py
+++
@@ -1,9 +0,0 @@
-"""
-Write a program that prints numbers from 1 to 100
-"""
-
-def print_numbers():
-    for i in range(1, 11):
-        print(i)
-
-print_numbers()
--- logic/02_square.py
+++
@@ -1,8 +0,0 @@
-"""
-Write a program that takes a number as imput and prints its square
-"""
-
-def square(n: int) -> None:
-    print(n * n)
-
-square(4)
