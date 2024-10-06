bad01d6ee5f70abc07996937585633b901f7376e
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-10-05 22:40:38 -0500

    tz

--- Logic/11_triangle.py
+++ Logic/11_triangle.py
@@ -4,7 +4,7 @@ Realizar un triangulo equilatero
 
 height = int(input("Enter the height of the triangle: "))
 
-def triangle(height) -> str:
+def triangle(height: int) -> None:
     for i in range(1, height + 1):
         print(' ' * (height - i) + '*' * (2 * i - 1))
         
