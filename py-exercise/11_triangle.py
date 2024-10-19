3d3748f5e3568eaa97493af830532fff15cfe836
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-10-18 20:45:36 -0500

    impar

--- py-exercise/11_triangle.py
+++ py-exercise/11_triangle.py
@@ -7,6 +7,6 @@ height = int(input("Enter the height of the triangle: "))
 def triangle(height: int) -> None:
     for i in range(1, height + 1):
         print(' ' * (height - i) + '*' * (2 * i - 1))
-        
+
 
 triangle(height)
---
+++ py-exercise/12_impar.py
@@ -0,0 +1,6 @@
+
+def impar():
+    x = [x for x in range(1, 20) if x % 2 != 0]
+    print(x)
+
+impar()
