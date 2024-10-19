fe9bc20c83a706a25c36b110e543fcfbe45446ac
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-10-18 19:39:09 -0500

    java init

---
+++ JavaExercise/HelloWorld/HelloWorld.java
@@ -0,0 +1,10 @@
+package JavaExercise.HelloWorld;
+
+/**
+ * Print hello world
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+}
