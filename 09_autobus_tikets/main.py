59dba8aefbe2f1331f3b3d71e1e7ffb43e3f5b2b
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-11-07 18:47:25 -0500

    autobus tikets init

---
+++ 09_autobus_tikets/main.py
@@ -0,0 +1,26 @@
+def main_menu():
+    choose_menu = [
+        "Login",
+        "Sign up",
+        "Trip Search",
+        "Book Trip",
+        "My Trips",
+    ]
+
+    for i, menu in enumerate(choose_menu):
+        print(f"{i + 1} {menu}")
+
+    while True:
+        try:
+            
+            pass
+        except:
+            pass
+    pass
+
+def main():
+    welcome = "Buy your tickets"
+    main_menu()
+
+if __name__ == "__main__":
+    main()
