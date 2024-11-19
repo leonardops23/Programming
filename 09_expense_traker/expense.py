686a711c81c9413218e440019c5c1c018da696ab
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2024-11-19 16:58:58 -0500

    serealize

--- 09_expense_traker/expense.py
+++ 09_expense_traker/expense.py
@@ -1,3 +1,8 @@
+from colorama import Fore
+from colorama import Style
+import json
+import os
+
 class Expense:
     def __init__(self, name, category, ammount):
         self.name = name
@@ -6,6 +11,7 @@ class Expense:
 
     def __repr__(self):
         return f"Expense: {self.name} - {self.category} - {self.ammount}"
+
 
 def get_name_expense() -> str:
     while True:
@@ -16,19 +22,20 @@ def get_name_expense() -> str:
             else:
                 return name_expense
         except Exception:
-            print("Try again")
+            print(f"{Fore.RED}Try again, Error Value{Style.RESET_ALL}")
 
 
 def get_ammount_expense() -> float:
     while True:
         try:
-            ammount_expense = float(input("Enter ammount of the expense: "))
+            ammount_expense = float(input("Enter ammount of the expense: $"))
             if ammount_expense < 0 or ammount_expense == '':
                 print("Don't save with value 0 or somenthing else")
             else:
                 return ammount_expense
         except ValueError:
-            print("Error, Value empty, Try again")
+            print(f"{Fore.RED}Try again, Error Value{Style.RESET_ALL}")
+
 
 
 def get_user_expense():
@@ -47,12 +54,12 @@ def get_user_expense():
     while True:
         print("\nCategory: 🔽")
         for i, category in enumerate(category_expense):
-            print(f"{i + 1} {category}")
+            print(f"    {i + 1} {category}")
 
         len_category = len(category_expense)
-        selected_range = f"[ 1 - {len_category} ]"
+        selected_range = f"[ 1 - {len_category} ] "
 
-        selected_index = int(input(f"Enter number according to the range {selected_range}"))
+        selected_index = int(input(f"Enter number according to the range {selected_range}")) - 1
         if selected_index in range(len(category_expense)):
             new_expense = category_expense[selected_index]
             return Expense(
@@ -64,9 +71,35 @@ def get_user_expense():
             print("Please, Try again")
 
 
-def save_to_file(datas_user):
-    print(datas_user)
-    
+def save_to_file(datas_user: Expense) -> bool:
+    save_data = "data.json"
+
+    if os.path.exists(save_data):
+        with open(save_data, "r") as file:
+            try:
+                data_list = json.load(file)
+            except:
+                data_list = []
+    else:
+        data_list = []
+
+    new_entry = {
+        "name": datas_user.name,
+        "category": datas_user.category,
+        "amount": datas_user.ammount,
+    }
+
+    data_list.append(new_entry)
+
+    with open(save_data, "w") as file:
+        json.dump(data_list, file)
+
+    if os.path.exists(save_data):
+        print(f"{Fore.GREEN}Successfully saved data{Style.RESET_ALL}")
+        return True
+    else:
+        print(f"{Fore.RED}Failed to save data{Style.RESET_ALL}")
+        return False
 
 def main():
     user_expense = get_user_expense()
