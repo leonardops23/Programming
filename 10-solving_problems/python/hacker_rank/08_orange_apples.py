78dcf70c04c6a9254295d31881d3c28f43c08fab
leonardo <ivanpachecoleo23@gmail.com>
2025-03-21 20:19:45 -0500

    new langueje to practice

---
+++ 10-solving_problems/python/hacker_rank/08_orange_apples.py
@@ -0,0 +1,19 @@
+
+#
+# Complete the 'countApplesAndOranges' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER s
+#  2. INTEGER t
+#  3. INTEGER a
+#  4. INTEGER b
+#  5. INTEGER_ARRAY apples
+#  6. INTEGER_ARRAY oranges
+#
+
+
+def countApplesAndOranges(s, t, a, b, apples, oranges):
+    # Write your code here
+    pass
+
+countApplesAndOranges()
