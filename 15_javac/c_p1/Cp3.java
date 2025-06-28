00caf62b22a1860ef5ec8a1609823da81bc388d5
Leonardo <ivanpachecoleo23@gmail.com>
2025-06-27 21:05:46 -0500

    sum two numbers

---
+++ 15_javac/c_p1/Cp3.java
@@ -0,0 +1,8 @@
+public class Cp3 {
+    // Sum of two numbers
+    public static void main(String[] args) {
+        int number1 = 23;
+        int number2 = 4;
+        System.out.println();
+    }
+}
