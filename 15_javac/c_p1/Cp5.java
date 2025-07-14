404c1204ebe5068b8c0c15cec4832523275df7e7
Leonardo <ivanpachecoleo23@gmail.com>
2025-07-14 17:49:14 -0500

    update projects

--- 15_javac/c_p1/Cp5.java
+++ 15_javac/c_p1/Cp5.java
@@ -1,3 +1,12 @@
 public class Cp5 {
-    
+    // Swap two numbers
+    public static void main(String[] args) {
+        int valor1 = 23;
+        int valor2 = 32;
+
+        int tem = valor2;
+
+        System.out.println("First Value: " + (valor1 = tem));
+        System.out.println("Second Value: " + (valor2 = valor1));
+    }
 }
---
+++ 15_javac/c_projects/01_Snake/VentanaJuego.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+public class VentanaJuego extends JFrame {
+    public VentanaJuego() {
+        setTitle("Juego de Snake");
+    }
+
+    public static void main(String[] args) {
+        VentanaJuego vetana = new VentanaJuego();
+        
+    }
+}
