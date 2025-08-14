703ab8a618df90da7ada1d24a917f17554d3e8a7
Ivan Pacheco <ivanpachecoleo23@gmail.com>
2025-08-14 16:55:48 -0500

    auth full

--- 17_djexpm/historical_facts/historical_facts/apps.py
+++
@@ -1,8 +0,0 @@
-from django.apps import AppConfig
-
-
-class HistoricalFactsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'historical_facts'
-    verbose_name = 'Hechos Históricos'
-
